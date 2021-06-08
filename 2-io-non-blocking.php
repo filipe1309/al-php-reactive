abfd2a0a82b936efad93114c7aaeca2cf3abad1f
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-07 21:24:14 -0300

    chore: io non blocking with stream_select

--- 2-io-non-blocking.php
+++ 2-io-non-blocking.php
@@ -5,8 +5,24 @@ $streamList = [
     fopen('2-file2.txt', 'r')
 ];
 
-foreach ($streamList as $stram) {
-    stream_set_blocking($stram, false);
+foreach ($streamList as $stream) {
+    stream_set_blocking($stream, false);
 }
 
-echo fgets($streamList[0]);
+do {
+    $copyReadStream = $streamList;
+    $numberOfChangedStreams = stream_select($copyReadStream, $write, $except, 0, 0);
+
+    // To avoid CPU high usage
+    if ($numberOfChangedStreams === 0) {
+        echo 'Do some async tasks here =)' . PHP_EOL;
+        continue;
+    }
+
+    foreach ($copyReadStream as $key => $stream) {
+        echo fgets($stream);
+        unset($streamList[$key]);
+    }
+} while (!empty($streamList));
+
+echo 'Read All Files =)' . PHP_EOL;
